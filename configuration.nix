35b64ca2ac95719bb0edde6bfb7b4d613d6db338
relzwit <kellenae129@gmail.com>
2025-05-25 10:39:30 -0400

    about to mess with audio

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./programs/stylix/stylix.nix 
+      #./programs/stylix/stylix.nix 
       ./programs/firefox/firefox.nix
     ];
   # enabling hyprland
@@ -57,6 +57,23 @@
 #      };
 #    };
 #  };
+
+
+
+
+#############
+### Audio ###
+#############
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 
   # music player daemon
   services.mpd = {
@@ -116,36 +133,6 @@
   };
 
 
-
-#old---:
-  # Enable sound with pipewire.
- # hardware.pulseaudio.enable = false;
- # security.rtkit.enable = true;
- # services.pipewire = {
- #   enable = true;
- #   alsa.enable = true;
- #   alsa.support32Bit = true;
- #   pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
- # };
-#new---:
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
@@ -191,6 +178,7 @@
     git
     gnugrep
     foot # terminal, i swear its not a fetish
+    tree # nice file hierarchy visualizations
 
 
     # messaging stuffs
@@ -206,6 +194,9 @@
     ncmpcpp
     jq
     mpc
+    alsa-utils
+    pulseaudioFull
+    pavucontrol
 
     steam
     libreoffice-qt6-fresh
